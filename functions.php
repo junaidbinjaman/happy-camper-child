60a7e731df19cdf29ece6f2689032724f2f8b69d
junaidbinjaman <me@junaidbinjaman.com>
2024-05-24 14:47:10 +0600

    Inital comit

---
+++ functions.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * The main function file of the theme
+ *
+ * @package    astra
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$theme   = wp_get_theme();
+$version = $theme->get( 'Version' );
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below.
+
+if ( ! function_exists( 'chld_thm_cfg_locale_css' ) ) :
+
+	/**
+	 * Enqueue parent style.
+	 *
+	 * @param string $uri the URL.
+	 * @return string
+	 */
+	function chld_thm_cfg_locale_css( $uri ) {
+		if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) ) {
+			$uri = get_template_directory_uri() . '/rtl.css';
+		}
+		return $uri;
+	}
+endif;
+
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+/**
+ * Enqueue css and js
+ *
+ * @return void
+ */
+function child_theme_configurator_scripts() {
+	wp_enqueue_style(
+		'chld_thm_cfg_child',
+		trailingslashit( get_stylesheet_directory_uri() ) . 'style.css',
+		array(
+			'astra-theme-css',
+			'woocommerce-layout',
+			'woocommerce-smallscreen',
+			'woocommerce-general',
+		),
+		fileatime( get_stylesheet_directory() . '/style.css' ),
+		'all'
+	);
+
+	wp_enqueue_script(
+		'chld_thm_cfg_child_scripts',
+		get_theme_file_uri() . '/scripts.js',
+		array( 'jquery' ),
+		fileatime( get_stylesheet_directory() . '/scripts.js' ),
+		true
+	);
+	wp_localize_script(
+		'chld_thm_cfg_child_scripts',
+		'wp_ajax',
+		array(
+			'url'   => admin_url( 'admin-ajax.php' ),
+			'nonce' => wp_create_nonce( 'hc_nonce' ),
+		)
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_scripts', 10 );
+
+// END ENQUEUE PARENT ACTION.
+
+/**
+ * The function inits all the shortcodes
+ *
+ * @return void
+ */
+function init_short_code() {
+	add_shortcode( 'happy_camper_url_mood_parameter', 'happy_camper_url_mood_parameter__callback' );
+	add_shortcode( 'get_greenhouse_title_color', 'get_greenhouse_title_color__callback' );
+	add_shortcode( 'get_greenhouse_title', 'get_greenhouse_title__callback' );
+	add_shortcode( 'cart_count', 'get_cart_count' );
+	add_shortcode( 'is_user_logged_in', 'is_user_logged_in__callback' );
+}
+
+add_action( 'init', 'init_short_code' );
+
+/**
+ * Returns mood URL parameter
+ *
+ * @return mixed
+ */
+function happy_camper_url_mood_parameter__callback() {
+	if ( isset( $_GET['mood'] ) ) { // phpcs:ignore
+		return sanitize_text_field( wp_unslash( $_GET['mood'] ) ); // phpcs:ignore
+	}
+	return 'happy';
+}
+
+/**
+ * Get color code based on the category assigned to the greenhouse product title
+ *
+ * The function checks the green house product category
+ * and echos the relevant color code.
+ *
+ * @return void
+ */
+function get_greenhouse_title_color__callback() {
+	$product_id = get_the_ID();
+	$terms      = get_the_terms( $product_id, 'product_cat' );
+	$reserved   = '#1d1e19';
+	$exotic     = '#919090';
+	$premium    = '#607fa4';
+	$house      = '#e8cb00';
+
+	foreach ( $terms as $term ) {
+		$term_id = $term->term_id;
+		switch ( $term_id ) {
+			case '103':
+				echo esc_html( $reserved );
+				break;
+			case '104':
+				echo esc_html( $exotic );
+				break;
+			case 105:
+				echo esc_html( $premium );
+				break;
+			case 106:
+				echo esc_html( $house );
+				break;
+		}
+	}
+}
+
+/**
+ * Get title based on the category assigned to the greenhouse product title
+ *
+ * The function checks the green house product category
+ * and echos the relevant title.
+ *
+ * @return void
+ */
+function get_greenhouse_title__callback() {
+	$product_id = get_the_ID();
+	$terms      = get_the_terms( $product_id, 'product_cat' );
+
+	foreach ( $terms as $term ) {
+		$term_id = $term->term_id;
+		switch ( $term_id ) {
+			case '103':
+				echo 'Reserved';
+				break;
+			case '104':
+				echo 'Exotic';
+				break;
+			case 105:
+				echo 'Premium';
+				break;
+			case 106:
+				echo 'House';
+				break;
+		}
+	}
+}
+
+/**
+ * Get the cart items total counts
+ *
+ * @return int
+ */
+function get_cart_count() {
+	if ( WC()->cart ) {
+		return WC()->cart->get_cart_contents_count();
+	}
+	return 0;
+}
+
+/**
+ * Shorten the woocommerce product title on archive boxes
+ *
+ * @param string $title The product title.
+ * @param int    $id The product id.
+ * @return string
+ */
+function shorten_woo_product_title( $title, $id ) {
+	if ( get_post_type( $id ) !== 'product' ) {
+		return $title;
+	}
+
+	if ( is_singular() ) {
+		return $title;
+	}
+
+	return wp_trim_words( $title, 3 );
+}
+
+add_filter( 'the_title', 'shorten_woo_product_title', 10, 2 );
+
+/**
+ * Removes dashboard and download menu.
+ *
+ * @param array $menu the menu listing.
+ * @return array
+ */
+function edit_woocommerce_account_menu_items( $menu ) {
+	$menu = array(
+		'orders'          => __( 'Orders', 'astra' ),
+		'edit-address'    => __( 'Address', 'astra' ),
+		'edit-account'    => __( 'Account Details', 'astra' ),
+		'customer-logout' => __( 'Logout', 'astra' ),
+	);
+
+	return $menu;
+}
+
+add_filter( 'woocommerce_account_menu_items', 'edit_woocommerce_account_menu_items' );
+
+/**
+ * The function skips the account dashboard and redirects to order listing tab
+ *
+ * @param Object $wp The wp object.
+ * @return void
+ */
+function skip_customer_dashboard( $wp ) {
+	if ( is_admin() ) {
+		return;
+	}
+
+	if ( 'my-account' === $wp->request ) {
+		wp_safe_redirect( site_url( 'my-account/orders' ) );
+		exit;
+	}
+}
+
+add_action( 'parse_request', 'skip_customer_dashboard' );
+
+/**
+ * The function rephrase the "Lost your password?" text
+ *
+ * @param string $translated_text The translated text.
+ * @param string $text The text to replace.
+ * @param string $domain The text domain.
+ * @return string
+ */
+function change_lost_your_password_text( $translated_text, $text, $domain ) {
+	if ( 'Lost your password?' === $text && 'woocommerce' === $domain ) {
+		$translated_text = 'Forgot your password?';
+	}
+
+	return $translated_text;
+}
+
+add_filter( 'gettext', 'change_lost_your_password_text', 20, 3 );
+
+/**
+ * The shortcode callback that returns My account
+ * if the current user is logged in. Otherwise, it returns Log In
+ *
+ * @return void
+ */
+function is_user_logged_in__callback() {
+	if ( is_user_logged_in() ) {
+		echo 'My Account';
+		return;
+	}
+
+	echo 'Log In';
+	return; //phpcs:ignore
+}
+
+/**
+ * Handle Password length
+ *
+ * @param array  $params Param.
+ * @param string $handle Handler.
+ * @return array
+ */
+function pwd_strength_meter_settings( $params, $handle ) {
+
+	if ( 'wc-password-strength-meter' === $handle ) {
+
+		$params = array_merge(
+			$params,
+			array(
+
+				'min_password_strength' => 2,
+				'i18n_password_error'   => 'Do not you want to be protected? Make it stronger!',
+				'i18n_password_hint'    => ' Please make your password <strong>at least 8 characters</strong> long , For more security use a mix of <strong>UPPER</strong> and <strong>lowercase</strong> letters, <strong>numbers</strong>, and <strong>symbols</strong>',
+			)
+		);
+
+	}
+	return $params;
+}
+
+add_filter( 'woocommerce_get_script_data', 'pwd_strength_meter_settings', 20, 2 );
---
+++ scripts.js
@@ -0,0 +1,284 @@
+(function ($) {
+    'use strict';
+
+    /**
+     * All of the code for public-facing JavaScript source
+     * should reside in this file.
+     *
+     * This enables to define handlers, for when the DOM is ready:
+     */
+
+    $(function () {
+        handleMegaMenuVisibility($);
+        mobileMegaMenuHandler($);
+        moodSelectorHandler($);
+		happyCamperAgeChecker();
+        storeEligibilityData($);
+        searchToolTipHandler($);
+
+        if (window.innerWidth >= 1024) {
+            productFilterToggleHandler($);
+        }
+
+        // Change the add to cart btn text on single product page
+        var btnText =
+            'Add To Cart <span class="dashicons dashicons-arrow-right-alt"></span>';
+        $('.single_add_to_cart_button').html(btnText);
+    });
+
+    $(document).on('elementor/popup/show', function (event, id, instance) {
+        /**
+         * Run productFilterToggleHandler function on mobile
+         * when the popup opens on greenhouse page
+         */
+        if (id === 2281) {
+            productFilterToggleHandler($);
+        }
+
+        /**
+         * Run productFilterToggleHandler function on mobile
+         * when the popup opens on all the shop page other than greenhouse
+         */
+        if (id === 2633) {
+            productFilterToggleHandler($);
+        }
+    });
+})(jQuery);
+
+/**
+ * UTILITY FUNCTIONS
+ *
+ * This section contains utility functions.
+ * These functions do not directly implement any features on the frontend by themselves.
+ * Rather, they support other functions by addressing common, smaller-scale problems,
+ * thereby facilitating the compvarion of larger tasks.
+ */
+
+/**
+ * Get the cookie value.
+ *
+ * The function accepts cookie name as an input
+ * and returns the cookie value.
+ *
+ * @param {string} name cookie name;
+ * @returns {string} cookie value
+ */
+function getCookie(name) {
+    var cookieArray = document.cookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookiePair = cookieArray[i].trim();
+        if (cookiePair.startsWith(name + '=')) {
+            cookiePair = cookiePair.substring(name.length + 1);
+            return cookiePair.slice(0, -1);
+        }
+    }
+    return null;
+}
+
+/**
+ * FEATURE FUNCTIONS
+ *
+ * This section contains feature functions that are directly responsible for handling specific features on the frontend.
+ * These functions implement the core functionality of user-facing elements, managing interactions and
+ * ensuring a responsive and dynamic user experience. They often utilize utility functions to handle
+ * sub-tasks, allowing for cleaner and more modular code structure.
+ */
+
+/**
+ * The function toggle the mega menu
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function handleMegaMenuVisibility($) {
+    var megaMenuContainer = $('.mega-menu-container');
+    var mainNavbar = $('.main-navbar');
+    var megaMenuNavItem = $('.menu-item-2749');
+
+    megaMenuContainer.hide();
+
+    megaMenuNavItem.on('mouseenter', function () {
+        megaMenuContainer.show();
+    });
+
+    megaMenuContainer
+        .on('mouseenter', function () {
+            megaMenuContainer.show();
+        })
+        .on('mouseleave', function () {
+            megaMenuContainer.hide();
+        });
+
+    mainNavbar.on('mouseleave', function () {
+        megaMenuContainer.hide();
+    });
+}
+
+/**
+ * The function toggle the mega menu on mobile
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function mobileMegaMenuHandler($) {
+    var mobileMegaMenu = 'mobile-mega-menu';
+    var megaMenuClasses = [
+        'greenhouse',
+        'bodega',
+        'spa',
+        'vapes',
+        'cannabinoid',
+        'feeling',
+    ];
+
+    $(document).on('elementor/popup/show', function (event, id, instance) {
+        if (id === 817) {
+            for (let i = 0; i < megaMenuClasses.length; i++) {
+                const menuClass = megaMenuClasses[i];
+                onPopupOpen(menuClass);
+            }
+        }
+    });
+
+    function onPopupOpen(menuClass) {
+        console.log(menuClass);
+        $(`.${mobileMegaMenu}-${menuClass}`).hide();
+
+        $(`.${mobileMegaMenu}-${menuClass}-btn`).on('click', function () {
+            $(`.${mobileMegaMenu}`).hide();
+            $(`.${mobileMegaMenu}-${menuClass}`).show();
+        });
+
+        $(`.${mobileMegaMenu}-${menuClass}-back-btn`).on('click', function () {
+            $(`.${mobileMegaMenu}-${menuClass}`).hide();
+            $(`.${mobileMegaMenu}`).show();
+        });
+    }
+}
+
+/**
+ * The function handles click events on mood selector btn on the home page
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function moodSelectorHandler($) {
+    $('.mood-selector-btn').on('click', function () {
+        var selectedMood = $(this).attr('data-mood');
+        localStorage.setItem('selectedModeOnHomePage', selectedMood);
+    });
+
+    var selectedMood = localStorage.getItem('selectedModeOnHomePage');
+
+    $(`.${selectedMood}-mood-selector-btn`).css({
+        'background-color': '#c3c1c0',
+        'border-radius': '50px',
+    });
+
+    $(`.${selectedMood}-mood-selector-btn img`).css({
+        border: '2px solid #005477',
+        'border-radius': '100%',
+        opacity: '1',
+    });
+}
+
+/**
+ * The function redirects user to age-check page when a users visit the site
+ * to ensures, the user is over 21.
+ *
+ * @returns void
+ */
+function happyCamperAgeChecker() {
+    var isEligible = getCookie('isEligible');
+    var path = location.pathname;
+
+    if (isEligible) {
+        jQuery('.hcAgeCheckerScreen').hide();
+        return;
+    };
+
+    if (
+        path !== '/age-check/' &&
+        path !== '/page-for-under-21/' &&
+        path !== '/terms-and-conditions/'
+    ) {
+         jQuery('.hcAgeCheckerScreen').show();
+    }
+}
+
+/**
+ * The function make the user eligible to browse the website
+ * when whe user clicks on the eligible btn
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function storeEligibilityData($) {
+    var eligibleCustomerBtn = '.happy-camper-eligible-customer-btn a';
+
+    $(document).on('click', eligibleCustomerBtn, function (event) {
+      document.cookie = 'isEligible=true; path=/; samesite=strict';
+        happyCamperAgeChecker();
+    });
+}
+
+/**
+ * The function handles the search container and buttons toggle
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function searchToolTipHandler($) {
+    var searchBtn = $('.header-secondary-menu ul > li:nth-child(1)');
+    var closeBtn = $('.header-secondary-menu ul > li:nth-child(2)');
+    var searchContainer = $('.search-form-container');
+
+    closeBtn.hide();
+    searchContainer.hide();
+
+    searchBtn.on('click', toggleVisibility);
+    closeBtn.on('click', toggleVisibility);
+
+    function toggleVisibility() {
+        searchBtn.toggle();
+        closeBtn.toggle();
+        searchContainer.toggle();
+    }
+
+    $(document).mouseup(function (e) {
+        if (
+            !searchContainer.is(e.target) &&
+            searchContainer.has(e.target).length === 0 &&
+            !searchBtn.is(e.target) &&
+            searchBtn.has(e.target).length === 0 &&
+            !closeBtn.is(e.target) &&
+            closeBtn.has(e.target).length === 0
+        ) {
+            searchBtn.show();
+            closeBtn.hide();
+            searchContainer.hide();
+        }
+    });
+}
+
+/**
+ * The function handles the product filter body toggle
+ *
+ * When a user clicks on the filter title/label or the icons,
+ * it toggles the filter listing
+ *
+ * @param {jQuery} $ jQuery reference
+ */
+function productFilterToggleHandler($) {
+    var filterCLassNamePrefix = ['tier', 'cannabinoid', 'vibe'];
+
+    for (let i = 0; i < filterCLassNamePrefix.length; i++) {
+        var prefix = filterCLassNamePrefix[i];
+
+        $(`.${prefix}-filter`)
+            .find('.product-filter-head ul > li:nth-child(1)')
+            .toggle();
+
+        $(`.${prefix}-filter .product-filter-head`).on('click', function () {
+            $(this).find('ul > li:nth-child(1)').toggle();
+            $(this).find('ul > li:nth-child(2)').toggle();
+
+            $(this).siblings().toggle();
+        });
+    }
+}
